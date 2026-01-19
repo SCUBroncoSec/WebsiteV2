b879a310b887ddf6a9b444d42494499b3e2146b5
Ethan Sychangco <94419010+ethansy878@users.noreply.github.com>
2026-01-18 18:27:25 -0800

    spotlight BS

--- src/app/BroncoCTF/layout.tsx
+++ src/app/BroncoCTF/layout.tsx
@@ -2,6 +2,17 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./styles-broncoctf.css";
 
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+
 export const metadata: Metadata = {
   title: "BroncoCTF",
   description: "SCU's cybersecurity competition",
@@ -22,12 +33,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body 
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >{children}</body>
     </html>
   );
 }
