f5633d10fafe9fa6d0fbcccce33e0b4a0f7f7fa5
Ethan Sychangco <esychangco@gmail.com>
2026-03-31 16:22:06 -0700

    Report

--- src/components/BroncoCTF/content.tsx
+++ src/components/BroncoCTF/content.tsx
@@ -10,6 +10,15 @@ const Content = () => {
     return (
         <div className="relative z-20">
 
+                        
+            <span className="pt-36 pb-20 flex flex-col items-center justify-center relative z-20 bg-linear-to-b from-red-600 to-red-950 bg-clip-text text-transparent text-7xl font-bold text-center">
+                2026 Report
+            </span>
+
+            <img src="report.png" alt="2026 Report" className="block mx-auto" />
+            
+
+
             <span className="pt-36 pb-20 flex flex-col items-center justify-center relative z-20 bg-linear-to-b from-red-600 to-red-950 bg-clip-text text-transparent text-7xl font-bold text-center">
                 About the Competititon
             </span>
@@ -18,10 +27,9 @@ const Content = () => {
                 BroncoCTF is an international CTF competition hosted by BroncoSec. It runs in conjunction with <a className="text-decoration: underline" href="https://hackforhumanity.io/">Hack for Humanity</a>, Santa Clara University's social good hackathon. <br /><br />
                 In BroncoCTF, teams of students solve various cybersecurity challenges by hunting for secret flags hidden in programs, cryptosystems, files, and even games! Use your ethical hacking skills to solve digital puzzles involving real-world technologies. <br /><br />
                 There are challenges for complete beginners and experienced hackers alike. Cash prizes are awarded to top Santa Clara University students, while the world can compete for bragging rights. <br /><br />
-                BroncoCTF 2026 will be our fifth CTF, and our third year hosting the event globally!
+                BroncoCTF 2026 was our fifth CTF, and our third year hosting the event globally!
             </TwoColumn>
 
-            
             <span className="pt-36 pb-20 flex flex-col items-center justify-center relative z-20 bg-linear-to-b from-red-600 to-red-950 bg-clip-text text-transparent text-7xl font-bold text-center">
                 Prize Pool
             </span>
